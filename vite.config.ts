8427dcd88e7e9a67963855192445e2afbd8504b5
sa1 <i@sa1.dev>
2024-04-10 13:23:37 +0530

    Update PWA icons purpose to "maskable" in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,13 +16,13 @@ export default defineConfig({
         orientation: "landscape",
         icons: [
           {
-            src: "/android-chrome-192x192.png",
+            src: "/android-chrome-192x192-maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "/android-chrome-512x512-maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
@@ -31,13 +31,11 @@ export default defineConfig({
             src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any",
           },
           {
             src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any",
           },
         ],
         screenshots: [
