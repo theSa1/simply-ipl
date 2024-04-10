6bf9f4e6aaf150f9566d383446a3af45aed88c02
sa1 <i@sa1.dev>
2024-04-10 12:58:47 +0530

    added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Simply IPL",
+        short_name: "IPL",
+        theme_color: "#273F87",
+        orientation: "landscape",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshot-1.jpg",
+            sizes: "1080x1920",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+          {
+            src: "/screenshot-2.jpg",
+            sizes: "1080x1920",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+        display: "fullscreen",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
